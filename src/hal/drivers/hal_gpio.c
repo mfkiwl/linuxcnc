15138feb4a1a811a7b61a660555d78b85bdcf86c
andypugh <andy@bodgesoc.org>
2023-07-26 14:45:58 +0100

    hal_gpio: New generic GPIO driver using gpiod.

---
+++ src/hal/drivers/hal_gpio.c
@@ -0,0 +1,176 @@
+/********************************************************************
+* Description:  hal_gpio.c
+*               GPIO driver for Rapberry Pi and similar using the
+* 		gpiod library
+*
+* Author: Andy Pugh
+* License: GPL Version 2+
+*
+* Copyright (c) 2023 All rights reserved.
+*
+*********************************************************************
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of version 2 of the GNU General
+    Public License as published by the Free Software Foundation.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    THE AUTHORS OF THIS LIBRARY ACCEPT ABSOLUTELY NO LIABILITY FOR
+    ANY HARM OR LOSS RESULTING FROM ITS USE.  IT IS _EXTREMELY_ UNWISE
+    TO RELY ON SOFTWARE ALONE FOR SAFETY.  Any machinery capable of
+    harming persons must have provisions for completely removing power
+    from all motors, etc, before persons enter any danger area.  All
+    machinery must be designed to comply with local and national safety
+    codes, and the authors of this software can not, and do not, take
+    any responsibility for such compliance.
+
+    This code was written as part of the LinuxCNC project.  For more
+    information, go to www.linuxcnc.org.
+*/
+
+#include "rtapi.h"		/* RTAPI realtime OS API */
+#include "rtapi_app.h"		/* RTAPI realtime module decls */
+#include "rtapi_slab.h"  	/* kmalloc() */
+#include "hal.h"		/* HAL public API decls */
+#include <gpiod.h>
+
+
+MODULE_AUTHOR("Andy Pugh");
+MODULE_DESCRIPTION("GPIO driver using gpiod / libgpiod");
+MODULE_LICENSE("GPL");
+
+// There isn't really any limit
+#define MAX_CHAN 64
+
+char *inputs[MAX_CHAN];
+RTAPI_MP_ARRAY_STRING(inputs, MAX_CHAN, "list of pins to use for input");
+char *outputs[MAX_CHAN];
+RTAPI_MP_ARRAY_STRING(outputs, MAX_CHAN, "list of pins to use for output");
+
+/***********************************************************************
+*                STRUCTURES AND GLOBAL VARIABLES                       *
+************************************************************************/
+
+/* this structure contains the runtime data needed by the
+   driver for a single port/channel
+*/
+
+typedef struct{
+	hal_u32_t *mode;
+	hal_bit_t *value;
+} hal_gpio_hal_t;
+    
+typedef struct {
+    hal_gpio_hal_t *hal;
+    struct gpiod_line *line;
+} hal_gpio_pin_t;
+
+typedef struct {
+    int num_inputs;
+    int num_outputs;
+    struct gpiod_chip *gpiod_chip;
+    hal_gpio_pin_t *inputs;
+    hal_gpio_pin_t *outputs;
+} hal_gpio_t;
+
+static int comp_id;
+static hal_gpio_t *gpio;
+
+/***********************************************************************
+*                  LOCAL FUNCTION DECLARATIONS                         *
+************************************************************************/
+
+static void hal_gpio_read(void *arg, long period);
+static void hal_gpio_write(void *arg, long period);
+
+/***********************************************************************
+*                      SETUP AND EXIT CODE                             *
+************************************************************************/
+
+int rtapi_app_main(void){
+    int retval;
+    char hal_name[HAL_NAME_LEN];
+
+    comp_id = hal_init("hal_gpio");
+    if (comp_id < 0) {
+        rtapi_print_msg(RTAPI_MSG_ERR, "hal_gpio: ERROR: hal_init() failed\n");
+        goto fail0;
+    }
+
+    // allocate shared memory for the base struct
+    gpio = (hal_gpio_t *)rtapi_kmalloc(sizeof(hal_gpio_t), RTAPI_GFP_KERNEL);
+    if (gpio == 0) {
+        rtapi_print_msg(RTAPI_MSG_ERR,
+                "hal_gpio component: Out of Memory\n");
+        goto fail0;
+    }
+
+    for (gpio->num_inputs = 0; inputs[gpio->num_inputs];gpio->num_inputs++) {}
+    gpio->inputs = (hal_gpio_pin_t *)rtapi_kmalloc(sizeof(hal_gpio_pin_t) * gpio->num_inputs, RTAPI_GFP_KERNEL);
+    for (gpio->num_outputs = 0; outputs[gpio->num_outputs];gpio->num_outputs++) {}
+    gpio->outputs = (hal_gpio_pin_t *)rtapi_kmalloc(sizeof(hal_gpio_pin_t) * gpio->num_outputs, RTAPI_GFP_KERNEL);
+
+    for (int i = 0; i < gpio->num_inputs; i++){
+	gpio->inputs[i].line = gpiod_line_find(inputs[i]);
+	if (gpio->inputs[i].line <= 0) {
+	    rtapi_print_msg(RTAPI_MSG_ERR, "The GPIO line %s can not be found %p\n", inputs[i], gpio->inputs[i].line);
+	    goto fail0;
+	}
+	gpiod_line_request_input(gpio->inputs[i].line, "linuxcnc");
+	gpio->inputs[i].hal = hal_malloc(sizeof(hal_gpio_hal_t));
+	retval = hal_pin_bit_newf(HAL_OUT, &(gpio->inputs[i].hal->value), comp_id, "hal_gpio.%s.in", inputs[i]);
+    }
+    for (int i = 0; i < gpio->num_outputs; i++){
+	gpio->outputs[i].line = gpiod_line_find(outputs[i]);
+	if (gpio->outputs[i].line <= 0) {
+	    rtapi_print_msg(RTAPI_MSG_ERR, "The GPIO line %s can not be found\n", outputs[i]);
+	    goto fail0;
+	}
+	gpiod_line_request_output(gpio->outputs[i].line, "linuxcnc", 0);
+	gpio->outputs[i].hal = hal_malloc(sizeof(hal_gpio_hal_t));
+	retval = hal_pin_bit_newf(HAL_IN, &(gpio->outputs[i].hal->value), comp_id, "hal_gpio.%s.out", outputs[i]);
+    }
+    
+    retval = rtapi_snprintf(hal_name, HAL_NAME_LEN, "hal_gpio.read");
+    retval = hal_export_funct(hal_name, hal_gpio_read, gpio, 0, 0, comp_id);
+    retval = rtapi_snprintf(hal_name, HAL_NAME_LEN, "hal_gpio.write");
+    retval = hal_export_funct(hal_name, hal_gpio_write, gpio, 0, 0, comp_id);
+    if (retval < 0){
+	rtapi_print_msg(RTAPI_MSG_ERR, "hal_gpio: failed to export functions\n");
+	goto fail0;
+    }
+    hal_ready(comp_id);
+    return 0;
+
+    fail0:
+    hal_exit(comp_id);
+    return -1;
+
+}
+/**************************************************************
+* REALTIME PORT READ/WRITE FUNCTION                                *
+**************************************************************/
+
+static void hal_gpio_read(void *arg, long period)
+{
+    hal_gpio_t *gpio = arg;
+    int i;
+    for (i = 0; i < gpio->num_inputs; i++){
+	*(gpio->inputs[i].hal->value) = gpiod_line_get_value(gpio->inputs[i].line);
+    }
+}
+
+static void hal_gpio_write(void *arg, long period)
+{
+    hal_gpio_t *gpio = arg;
+    int i;
+    for (i = 0; i < gpio->num_outputs; i++){
+	gpiod_line_set_value(gpio->outputs[i].line, *(gpio->outputs[i].hal->value));
+    }
+}
